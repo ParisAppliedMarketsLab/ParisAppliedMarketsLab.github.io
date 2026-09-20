18f37fd54c24c26afd2c472227d63a8e2aa695db
Thomas FAVOLI <115409285+tf83f@users.noreply.github.com>
2026-09-20 20:04:11 +0200

    Add files via upload

--- logo.py
+++ logo.py
@@ -10,32 +10,35 @@ grid_size = 3 * cell + 2 * gap
 x0 = (SIZE - grid_size) // 2
 y0 = (SIZE - grid_size) // 2
 
-D, M, L, W = 135, 150, 165, 250
-DARK   = (D, D, D, 255)
-MEDIUM = (M, M, M, 255)
-LIGHT  = (L, L, L, 255)
-WHITE  = (W, W, W, 0)
+
 BORDER = (35, 35, 35, 255)
 
 BORDER_WIDTH = 3
 CORNER_RADIUS = 35
 
-for i in range(3):
-    for j in range(3):
-        color = WHITE
-        if   i == 0 and j == 0: color = DARK
-        elif i == 1 and j == 1: color = MEDIUM
-        elif i == 2 and j == 2: color = LIGHT
+A = "#60B345"
+B = "#B0C223" 
+C = "#FFD100"
+D = "#F47920"
+E = "#E23838"
+F = "#A93265" 
+G = "#6F2C91" 
+H = "#3868BB"
+I = "#00A4E4"
 
-        x = x0 + j * (cell + gap)
-        y = y0 + i * (cell + gap)
+colors = [(I, 0,0), (H, 0,1), (F, 0,2), (C, 1,0), (E, 1,1), (G, 1,2),(D, 2,0), (B, 2,1), (A,2,2)]
 
-        draw.rounded_rectangle(
-            [x, y, x + cell, y + cell],
-            radius=CORNER_RADIUS,
-            fill=color,
-            outline=BORDER,
-            width=BORDER_WIDTH,
+for color, i, j in colors:
+
+    x = x0 + j * (cell + gap)
+    y = y0 + i * (cell + gap)
+
+    draw.rounded_rectangle(
+        [x, y, x + cell, y + cell],
+        radius=CORNER_RADIUS,
+        fill=color,
+        outline=BORDER,
+        width=BORDER_WIDTH,
         )
 
 img.save("./logo.png")  
